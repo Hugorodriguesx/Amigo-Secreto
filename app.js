10bdac1e9e29b112ba0df63e6dd2fada0145fefb
Hugorodriguesx <vector.rodrigues.olv@gmail.com>
2025-01-29 17:43:14 -0300

    Primeira atualização do Projeto Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    if (nome === "" || amigos.includes(nome)) {
+        alert("Nome inválido ou já adicionado.");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";
+}
+
+function atualizarLista() {
+    const lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpa a lista antes de atualizar
+
+    amigos.forEach((amigo) => {
+        const item = document.createElement("li");
+        item.textContent = amigo;
+
+        const NomeAdicionadosNaTela = document.createElement(amigo);
+        
+        
+        item.appendChild(NomeAdicionadosNaTela);
+        lista.appendChild(item);
+    }); // Adiciona os nomes adicionados na lista na Tela
+}
